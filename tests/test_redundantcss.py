32448c6ce1b0415266472ceeea6651cf3b65adb5
Josh H <josh@securesap.com>
2024-05-01 14:40:08 +0100

    Create Tests

--- tests/test_redundantcss.py
+++ tests/test_redundantcss.py
@@ -1,48 +1,84 @@
-from redundantcss.redundantcss import get_html_classes, get_css_classes, compare_classes
-from redundantcss.validateArgs import parse_classes, check_folder_contents
-
+import pytest
+from redundantcss import redundantcss
+from pathlib import Path
 
 # Global variables for file paths
 # Will be used in many tests
 
-# Exact path to single html file
-TEMPLATE_PATH = "template1.html"
-
-# Path to folder with multiple files, ONLY html files
-TEMPLATE_FOLDER = "templates/"
-
-# Folder with multiple file types including HTML and CSS
-MIXED_FOLDER = "mixed_files/"
-
-# Exact path to stylesheet
-STYLESHEET = "styles.css"
-
 # USES PYTEST
+# set paths as global variables
+TEMPLATE_PATH = "tests/template1.html"  # Exact path to single html file
+MULTI_PATH = ["tests/template1.html", "tests/templates/_layout.html"]  # Paths to multiple HTML files
+TEMPLATES_FOLDER_PATH = "tests/templates"  # Path to folder with multiple files, ONLY html files
+STYLESHEET_PATH = "tests/styles.css"  # Exact path to stylesheet
+
+# validate_args.py function check_folder_contents(path) uses
+# Path object from pathlib library. Iterates over each .html
+# file within directory.
+FOLDER_WITH_HTML = Path("tests/mixed_with_html")  # Folder with multiple file types, NOT including HTML files
+FOLDER_WITHOUT_HTML = Path("tests/mixed_without_html")  # Folder with multiple file types, NOT including HTML files
+
 def main():
-    pass
+    test_single_template_files()
+    test_parses_templates_folder()
+    test_parses_stylesheet()
+    test_class_comparison()
+
 
 # Tests for redundantcss.py
 # --
-# Checks single '.html' file
-def test_single_template_file():
-    with open(TEMPLATE_PATH, 'r') as file
-    assert get_html_classes(file) == ['center-title', 'song-dropdown', '
-
-
-# Checks multiple '.html' files passed as argument to 'get_html_classes()'
-def test_multi_template_files():
-    pass
+# Checks for single or multiple '.html' files passed as arguments.
+def test_single_template_files():
+    assert redundantcss.get_html_classes(TEMPLATE_PATH) == {
+        'bg-container', 'green-text', 'protest'
+        }
+    
+    assert redundantcss.get_html_classes(MULTI_PATH) == {
+        'bg-container', 'green-text', 'protest', 'page-container'
+        }
 
 
 # Make sure a folder name passed as argument to function parses correctly and
 # each '.html' file in the folder is read.
 def test_parses_templates_folder():
-    pass
+    assert redundantcss.get_html_classes(TEMPLATES_FOLDER_PATH) == {
+        'bg-container', 'green-text', 'protest', 'page-container'
+        }
 
 
 # Ensure the function is returning all of the classes created
 # in the 'styles.css' or stylesheet given to 'get_css_classes()'.
 def test_parses_stylesheet():
-    pass
+    assert redundantcss.get_css_classes(STYLESHEET_PATH) == ['page-container', 'bg-container', 'title-text', 'protest', 'unused-class']
 
-# Create tests for validatArgs.py
+
+def test_class_comparison():
+    css_classes = ['page-container', 'bg-container', 'title-text', 'protest', 'unused-class']
+    html_classes = ['page-container', 'bg-container', 'title-text', 'protest']
+    assert redundantcss.compare_classes(css_classes, html_classes) == ["unused-class"]
+
+
+# Create tests for validateArgs.py
+# Tests for validate_args.py
+# --
+# Tests function used within get_html_classes().
+def test_parse_classes():
+    # This function can only accept 1 html sheet at a time.
+    # Error handling is done by parent function
+    # Error handling being added to this function at a later stage
+    with open(TEMPLATE_PATH, 'r') as html_sheet:
+        assert redundantcss.parse_classes(html_sheet) == {
+        'bg-container', 'green-text', 'protest'
+        }
+
+
+def test_no_html_folder():
+    assert redundantcss.check_folder_contents(FOLDER_WITHOUT_HTML) == False
+
+
+def test_mixed_folder():
+    assert redundantcss.check_folder_contents(FOLDER_WITH_HTML) == True
+
+
+if __name__ == "__main__":
+    main()
